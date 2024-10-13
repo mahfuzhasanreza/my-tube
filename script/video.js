1404bfb437b7047d2a7d40f2774a62615fe8fe21
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-13 11:35:42 +0600

    Load Videos And Display Cards On The Ui

--- script/video.js
+++ script/video.js
@@ -5,6 +5,13 @@ const loadCategories = () => {
         .catch(error => console.log(error))
 }
 
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch(error => console.log(error))
+}
+
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById('categories');
 
@@ -19,4 +26,49 @@ const displayCategories = (categories) => {
     })
 }
 
+const cardDemo = {
+    "category_id": "1001",
+    "video_id": "aaab",
+    "thumbnail": "https://i.ibb.co/QPNzYVy/moonlight.jpg",
+    "title": "Midnight Serenade",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/fDbPv7h/Noha.jpg",
+            "profile_name": "Noah Walker",
+            "verified": false
+        }
+    ],
+    "others": {
+        "views": "543K",
+        "posted_date": ""
+    },
+    "description": "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
+};
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos');
+
+    videos.forEach(video => {
+        console.log(video);
+        const card = document.createElement('div');
+        card.classList = 'card card-compact';
+        card.innerHTML = `
+        <figure>
+            <img
+            src=${video.thumbnail}
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+        videoContainer.append(card);
+    })
+}
+
 loadCategories();
+loadVideos();
